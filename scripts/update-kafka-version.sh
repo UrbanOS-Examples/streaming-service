b61c828afb14f4d408c6d4b12a12840b8e376308
JarredOlson <jarred.olson@accenture.com>
2019-09-25 14:58:38 -0400

    Removing the streaming public kafka cluster

--- scripts/update-kafka-version.sh
+++ scripts/update-kafka-version.sh
@@ -3,7 +3,6 @@ set -e
 
 echo
 echo "Deleting old Stateful Sets:"
-kubectl delete sts streaming-service-public-kafka -n streaming-public --cascade=false
 kubectl delete sts streaming-service-kafka -n streaming-prime --cascade=false
 
 start_time="$(date -u +%s)"
@@ -20,16 +19,6 @@ end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"
 echo "Prime STS Recreated in $elapsed seconds"
 
-until kubectl get sts streaming-service-public-kafka -n streaming-public 2> /dev/null
-do
-  printf "."
-  sleep 10s
-done
-end_time="$(date -u +%s)"
-elapsed="$(($end_time-$start_time))"
-echo "Public STS Recreated in $elapsed seconds"
-
 /bin/bash ./scale_down.sh
 
 kubectl annotate statefulset streaming-service-kafka strimzi.io/manual-rolling-update=true -n streaming-prime
-kubectl annotate statefulset streaming-service-public-kafka strimzi.io/manual-rolling-update=true -n streaming-public
